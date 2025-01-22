e536aa728bac93dc69886d9d17f378611f055a27
Your Name <your-email@example.com>
2025-01-22 19:15:55 +0600

    add pylint and also workflows

---
+++ python-fast-app/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Update package lists and install necessary packages
+apt-get update && apt-get install -y python3-pip python3-venv
+
+# Create and activate the virtual environment
+python3 -m venv venv
+. venv/bin/activate
+
+# Install dependencies from requirements.txt
+# pip install -r /pyapp/requirements.txt
