5e11fd676fc109ed7d95d067dd72d3805e711611
riyad20038 <riyadcreatives@gmail.com>
2025-02-20 17:40:22 +0600

    ec2 udate workflow

--- python-fast-app/install.sh
+++ python-fast-app/install.sh
@@ -4,8 +4,7 @@
 apt-get update && apt-get install -y python3-pip python3-venv
 
 # Create and activate the virtual environment
-python3 -m venv venv
-. venv/bin/activate
+python3 -m venv /pyapp/venv
 
 # Install dependencies from requirements.txt
 # pip install -r /pyapp/requirements.txt
